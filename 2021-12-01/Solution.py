8a3e4a38c684f0ff653e4600c0546e10263d7d60
Rook42 <r.cookdev@gmail.com>
2021-12-13 15:42:29 -0500

    Delete 2021-12-01 directory
    Removed redundant 198 old name

--- 2021-12-01/Solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def rob(self, nums):
-        preprior, prior, current = 0, 0, 0
-        for i in nums:
-            current = max(prior, i + preprior)
-            preprior = prior
-            prior = current
-        return current
